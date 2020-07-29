f29ab4c1283cfa0e95e06da9359d569c9eabfa9d
Thiago de Melo Carvalho <tmcnet@ThiagoMBP.local>
2020-07-29 20:29:50 -0300

    Aula 2 - Roteamento e State

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/img/Logo.png';
 import './Menu.css';
 // import ButtonLink from './components/ButtonLink';
@@ -7,11 +8,11 @@ import Button from '../Button';
 function Menu () {
     return (
         <nav className="Menu">
-            <a href="/">  
+            <Link to="/">  
                 <img className="Logo" src={Logo} alt="ThiFlix Logo"/>
-            </a>
+            </Link>
 
-            <Button as="a" className="ButtonLink" href="/">
+            <Button as={Link} className="ButtonLink" to="/cadastro/video">
                 Novo video
             </Button>
         </nav>
